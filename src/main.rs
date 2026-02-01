31f98fa948413865cc84c212054d903bdac3072b
jesperkha <hammerjesp@gmail.com>
2026-02-01 13:21:18 +0100

    Remove unused stdlib and thirdparty paths

--- src/main.rs
+++ src/main.rs
@@ -57,8 +57,6 @@ target = "x86-64" # Target arch (x86-64)
 
 [options]
 debug-mode = false
-stdlib-path = ":root:/lib/std"         
-thirdparty-path = ":root:/lib/thirdparty"
 "#;
 
     write_file("koi.toml", content)?;
